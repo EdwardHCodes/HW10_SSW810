9e488bb2340fe40275d6b239d283812a81c0dbb5
EdwardHCodes <38704768+EdwardHCodes@users.noreply.github.com>
2020-08-11 19:46:52 -0500

    Update Student_Repository.py

--- Student_Repository.py
+++ Student_Repository.py
@@ -27,55 +27,110 @@ Major RequiredCourse
 #Class to contain the Majors
 class Major:
     #pt header row to help generate pretty table
+    passing_grades: List[str] = ("A", "A-", "B+", "B", "B-", "C+", "C", "C-")
+    numerical_grades: Dict[str, float] = {
+    'A': 4.0, 'A-': 3.75,
+
+    'B+': 3.25, 'B': 3.0, 'B-': 2.75,
+
+    'C+': 2.25, 'C': 2.0, 'C-': 0,
+
+    'D+': 0, 'D': 0, 'D-': 0,
+
+    'F': 0}
+    #To calculate Student GPA
+    #Take Average of the the students courses divided by the total possible score
+    #student takes 5 courses and makes B, 3.0, 15.0/20.0
     pt_hdr: Tuple[str, List[str], List[str]] = ("Major", "Required Courses", "Elective Courses")
     
     #initializes a class of major
     def __init__(self, major: str):
-        self.major = major
-        self.req: List[str] = list()
-        self.ele: List[str] = list()
+        self.major: str = major
+        self.req: Set[str] = set()
+        self.ele: Set[str] = set()
     
     #need a way to add a course
     #appears that the format
     def add_course(self, course: str, isreq: str):
-        self.major[course] = course
-    
-    def isreq(self):
-        self.req = req
-        self.ele = ele
+        if isreq == "R":
+            self.req.add(course)
+        else:
+            self.ele.add(course)
 
     def pt_row(self) -> Tuple[str, List[str], List[str]]:
-        return self.major, self.req_courses, self.ele_courses
+        return self.major, sorted(self.req), sorted(self.ele)
+
+    #Remainig REquired Courses
+    #what do we need to know
+    #What courses did the student take, and what grade did they make?
+    def remain_req(self, courses: Dict[str, str]) -> List[str]:
+        passed_courses: Dict[str, str] = self.passed(courses)
+        return sorted(self.req - set(passed_courses.keys()))
+    #REmaining Elective COurses
+    def remain_elec(self) -> List[str]:
+        passed_courses: Dict[str, str] = self.passed(courses)
+        #are there any courses in common between self.req and pass_courses.keys()
+        if self.ele.intersection(set(passed_courses.keys())):
+            return list()
+        else:
+            return sorted(self.ele)
+
+    #GPA
+    def gpa(self, courses: Dict[str, str]) -> float:
+        number_courses: int = 0
+        total_points: float = 0.0
+        for grade in courses.values():
+            if grade in Major.numerical_grades:
+                total_points += Major.numerical_grades[grade]
+                number_courses += 1
+        if number_courses == 0:
+            return 0.0
+        else:
+            return total_points/number_courses
+            
+            
+        
+    def passed(self, courses: Dict[str, str]) -> Dict[str, str]:
+        passing_dict: Dict[str, str] = dict()
+        for course, grade in courses.values():
+            if grade in passing_grades:
+                passing_dict[course] = grade
+        return passing_dict
+        
 
 #Student Class to store information about each student
 #I worked on this while re-listening to the video, I
 #I am still trying to wrap my head around actually implementing public/private methods
 class Student:
-    pt_hdr: Tuple[str, str, str, str, str] = ("CWID", "Name", "Major", "Completed Courses", "Required Courses", "GPA")
-    passing_grades: List[str] = ("A", "A-", "B+", "B", "B-", "C+", "C", "C-")
-    def __init__(self, CWID: int, Name: str, Major: str) -> None:
-        self.cwid: int = CWID
-        self.name: str = Name
-        self.major: str = Major
-        self.courses: Dict[str, str] = dict() #NOT A DEFAULT DICTIONARY 
+    pt_hdr: Tuple[str, str, str, List[str], List[str]] = ("CWID", "Name", "Major", "Completed Courses", "Remaining Required", "Remaining Electives", "GPA")
+    def __init__(self, cwid: int, name: str, major: Major) -> None:
+        self.cwid: int = cwid
+        self.name: str = name
+        self.major: str = major
+        self.courses: Dict[str, str] = dict() #NOT A DEFAULT DICTIONARY Key:Value Course:Grade
 
     def __str__(self) -> str: #Trying to not just copy the code, and want to try and differientiate my solution.
-        return f"{Name} has the student ID of {CWID} and is a {Major}"
+        return f"{name} has the student ID of {cwid} and is a {major}"
 
     def add_course(self, course: str, grade: str) -> None:
         self.courses[course] = grade
     
-    def pt_row(self) -> Tuple[str, str, List[str]]:
+    def pt_row(self) -> Tuple[str, str, str, List[str], List[str], List[str], float]:
         """return a list of values to populate the prettytable for this student"""
-        return self.cwid, self.name, sorted(self.courses.keys())
-    
+        return self.cwid, self.name, self.major, sorted(self.major.passed(self.courses).keys()),\
+            sorted(self.courses.keys()), self.major.remain_req(self.courses), self.major.remain_ele(self.courses), self.major.gpa(courses)
+
+    ##Three checks, is it required, is it elected, GPA self.num self.denom 
+    ##calculate remaining required, GPA
+
+
 #Expression to check for grades
 class Instructor:
     pt_hdr: Tuple[str, str, str] = ("CWID", "Name", "Completed Courses")
-    def __init__(self, CWID: int, Name: str, Courses: dict, Department: str):
-        self.cwid: int = CWID
-        self.name: str = Name
-        self.dept: str = Department
+    def __init__(self, cwid: int, name: str, courses: dict, department: str):
+        self.cwid: int = cwid
+        self.name: str = name
+        self.dept: str = department
         self.courses: DefaultDict[str, int] = defaultdict(int)  # key: course value: number of students
 
     def add_student(self, Name: str, Course: str, Grade: str): ##Why do we not need to add the grade of the course that the student obtained...?
@@ -91,14 +146,15 @@ class Instructor:
 class Repository:
     def __init__(self, dir_path: str, ptables: bool=True):
         self.dir_path: str = dir_path
-        self.Student: str = Dict[str, Student] = dict() #This is the string and an instance of class student,
-        self.instructor: str = Dict[str, instructor] = dict()
+        self.students: Dict[str, Student] = dict() #This is the string and an instance of class student,
+        self.instructors: Dict[str, Instructor] = dict()
+        self.majors: Dict[str, Major] = dict() ##
         # I am a little unfamiliar with how we are mapping this self.student and self.instructor because we dont pass those in as parameters in the __init__ method.
         try:
+            self.get_majors(os.path.join(dir_path, 'majors.txt'))
             self.get_students(os.path.join(dir_path, 'students.txt'))
             self.get_instructors(os.path.join(dir_path, 'instructor.txt'))
             self.get_grades(os.path.join(dir_path, 'grades.txt'))
-            self.get_majors(os.path.join(dir_path, 'majors.txt'))
         except ValueError as ve:
             print(ve)
         except FileNotFoundError as fnfe:
@@ -113,7 +169,9 @@ class Repository:
     def get_majors(self, path:str) -> None:
         """gets majors and required/elective classes"""
         for major, required, course in file_reader(path, 3, sep='\t', header=False):
-            self.major[major] = Major(major, required, course)
+            if major not in self.majors:
+                self.majors[major] = Major(major)
+            self.majors[major].add_course(course, isreq)
 
     def get_students(self, path: str) -> None:
         """
